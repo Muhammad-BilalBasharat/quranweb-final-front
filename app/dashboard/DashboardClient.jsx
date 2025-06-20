9c03031f591a06eb0778ee57b4eac40efa4c97af
Jahan Zaib <j.haider5x5@gmail.com>
2025-06-20 11:43:59 -0700

    Dashboard Login Problem Solved

--- app/dashboard/DashboardClient.jsx
+++ app/dashboard/DashboardClient.jsx
@@ -3,17 +3,24 @@
 import { useAuthStore } from "@/api/authStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 export default function DashboardClient() {
   const user = useAuthStore((state) => state.user);
   const token = useAuthStore((state) => state.token);
+  const setToken = useAuthStore((state) => state.setToken);
+  const setUser = useAuthStore((state) => state.setUser);
   const router = useRouter();
 
   useEffect(() => {
-    if (!user && !token) {
+    const cookieToken = Cookies.get("auth_token");
+    if (!token && cookieToken) {
+      setToken(cookieToken);
+    }
+    if (!user && !cookieToken) {
       router.replace("/login");
     }
-  }, [user, token, router]);
+  }, [user, token, setToken, setUser, router]);
 
   if (!user) return null;
   return (
